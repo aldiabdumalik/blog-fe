1e3544c23e31bd48f997a6ab2f0f953e2eba8592
Abdu <aldiabdumalik98@gmail.com>
2023-12-20 20:45:27 +0700

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,6 +2,31 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className='flex w-full bottom-0'>Footer</footer>
+    <footer className='flex flex-col w-full bottom-0 px-2 md:px-4 lg:px-28 pt-8 pb-2 gap-2 shadow'>
+      <div className='flex flex-col md:flex-row md:gap-8 md:justify-between items-start border-b border-dark pb-2'>
+        <div className="flex flex-col flex-1 gap-2">
+          <h2 className='text-2xl text-darkBody font-bold'>INKAR</h2>
+          <p className='text-sm text-darkBody'>
+            Website ini menyajikan berbagai macam informasi mulai dari keseharian sampai sepak bola ada disini
+          </p>
+        </div>
+        <div className="flex flex-col flex-1 gap-2">
+          <h2 className='text-2xl text-darkBody font-bold'>Hubungi Kami</h2>
+          <p className='text-sm text-darkBody'>Kirim Pesan</p>
+          <p className='text-sm text-darkBody'>WhatsApp</p>
+          <p className='text-sm text-darkBody'>hi@inkar.com</p>
+        </div>
+        <div className="flex flex-col flex-1 gap-2">
+          <h2 className='text-2xl text-darkBody font-bold'>Tautan</h2>
+          <p className='text-sm text-darkBody'>About</p>
+          <p className='text-sm text-darkBody'>Privacy Policy</p>
+          <p className='text-sm text-darkBody'>hi@inkar.com</p>
+        </div>
+      </div>
+      <div className="flex w-full justify-start">
+
+        <h4 className='text-darkBody font-semibold text-sm'>&copy; Copyright 2024 - Team Inkar</h4>
+      </div>
+    </footer>
   )
 }
